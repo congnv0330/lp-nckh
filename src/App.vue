cbd01d829604092e8a44e47dc852d005ed2c5335
Nguyễn Văn Công <congnguyenlop9a1@gmail.com>
2020-04-16 11:20:15 +0700

    Initial Project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="min-h-screen w-full flex flex-col bg-gray-100 px-5 md:px-20">
+    <main class="w-full py-5 my-auto">
+      <router-view/>
+    </main>
+    <footer class="text-center md:text-left py-5">
+      © 2020 NCH - NCKH, Linear programming
+    </footer>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'MainLayout'
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1 class="text-2xl text-center md:text-3xl md:text-left font-semibold">Quy hoạch tuyến tính</h1>
+    <p class="text-center md:text-left text-gray-700 mb-5">
+      Quy hoạch tuyến tính là bài toán tối ưu hóa, trong đó hàm mục tiêu và các điều kiện ràng buộc đều là tuyến tính.
+    </p>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <template v-for="(item, index) in items">
+        <div :key="index" class="bg-white rounded shadow-md">
+          <div class="p-5">
+            <div class="font-semibold text-xl mb-1">{{ item.title }}</div>
+            <p class="text-gray-700 text-base mb-3">{{ item.description }}</p>
+            <button class="bg-blue-500 hover:bg-blue-700 focus:outline-none focus:shadow-outline text-white py-2 px-4 rounded">
+              Xem &raquo;
+            </button>
+          </div>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      items: [
+        {
+          title: 'Chương 1',
+          description: 'Bài toán quy hoạch tuyến tính',
+          route: ''
+        },
+        {
+          title: 'Chương 2',
+          description: 'Bài toán đối ngẫu',
+          route: ''
+        },
+        {
+          title: 'Chương 3',
+          description: 'Bài toán vận tải',
+          route: ''
+        },
+        {
+          title: 'Chương 4',
+          description: 'Phương án sơ đồ mạng',
+          route: ''
+        }
+      ]
+    }
   }
 }
 </script>
