02b28051545cba8dc7c8f8816cad31dcbcb9e050
Cuong <duccuong9a9@gmail.com>
2020-04-23 20:29:45 +0700

    Edited structure
    Use for both C1 and C2

--- src/core/c1.js
+++ src/core/c1.js
@@ -3,16 +3,23 @@
 import PhanSo from './modules/phanso'
 import { decimalToFraction } from './utils/fraction'
 
-const hsM = []
-const pa = []
-const d = []
-let m = 0
-let n = 0
-const subVar = []
-const fakeVar = []
-let fx
-let rb
-let orginN = n
+class LinearProgramming {
+  constructor () {
+    this.fx = []
+    this.rb = []
+    this.rbdau = []
+    this.n = 0
+    this.m = 0
+    this.orginN = 0
+    this.pa = []
+    this.hsM = []
+    this.d = []
+    this.subVar = []
+    this.fakeVar = []
+    this.freeVar = []
+    this.type = ''
+  }
+}
 
 const failed = {
   success: false,
@@ -27,29 +34,25 @@ function Multi (p1, p2) { const mul = new PhanSo(); mul.tu = p1.tu * p2.tu; mul.
 
 function Div (p1, p2) { const div = new PhanSo(); div.tu = p1.tu * p2.mau; div.mau = p1.mau * p2.tu; div.RutGon(); return div }
 
-function inputFx (input) {
-  const fxString = input.trim()
+function input (inputFx, inputMatrix, inputRB, fxType) {
+  const orgin = new LinearProgramming()
+  const fxString = inputFx.trim()
   const fxOrgin = fxString.split(' ')
-  const fx = []
   for (let i = 0; i < fxOrgin.length; i++) {
     const tmp = fxOrgin[i].split('/')
     if (tmp.length === 2) {
-      fx[i] = new PhanSo(parseFloat(tmp[0]), parseFloat(tmp[1]))
+      orgin.fx[i] = new PhanSo(parseFloat(tmp[0]), parseFloat(tmp[1]))
     } else {
-      fx[i] = new PhanSo(parseFloat(tmp[0]))
+      orgin.fx[i] = new PhanSo(parseFloat(tmp[0]))
     }
   }
-  n = fxOrgin.length
-  return fx
-}
+  orgin.n = fxOrgin.length
+  orgin.orginN = orgin.n
 
-function inputMatrix (inputMatrix, inputRB, fxType) {
   const matrixrb = inputMatrix
   const matrixArr = matrixrb.split('\n')
-  m = matrixArr.length
+  orgin.m = matrixArr.length
   let maxLine = 0
-  const rbdau = []
-  const rb = []
 
   let RBin = inputRB.trim()
   RBin = RBin.split(';')
@@ -59,194 +62,215 @@ function inputMatrix (inputMatrix, inputRB, fxType) {
     const len = tmp.length - 2
     for (let j = 0; j < len; j++) {
       const col = parseInt(tmp[j]) - 1
-      rbdau[col] = 1
+      orgin.rbdau[col] = 1
       if (tmp[len] === '<=') {
-        rbdau[col] = -1
+        orgin.rbdau[col] = -1
       }
     }
   }
 
-  let all = 0
-  for (let i = 0; i < n; i++) {
-    if (rbdau[i] === -1) {
-      fx[i].tu *= -1
-    } else if (rbdau[i] === undefined) {
-      all += 1
-    }
-  }
-
-  for (let i = 0; i < m; i++) {
+  for (let i = 0; i < orgin.m; i++) {
     matrixArr[i] = matrixArr[i].trim()
     const line = matrixArr[i].split(' ')
     maxLine = Math.max(maxLine, line.length - 2)
-    rb[i] = []
+    orgin.rb[i] = []
     for (let j = 0; j < line.length - 2; j++) {
       const tmp = line[j].split('/')
       if (tmp.length === 2) {
-        rb[i][j] = new PhanSo(parseFloat(tmp[0]), parseFloat(tmp[1]))
+        orgin.rb[i][j] = new PhanSo(parseFloat(tmp[0]), parseFloat(tmp[1]))
       } else {
-        rb[i][j] = new PhanSo(parseFloat(tmp[0]))
-      }
-      if (rbdau[j] === -1) {
-        rb[i][j].tu *= -1
-      }
-      if (rbdau[j] === undefined) {
-        rb[i][j + orginN] = new PhanSo(rb[i][j].tu * -1)
-        fx[j + orginN] = new PhanSo(fx[j].tu * -1)
+        orgin.rb[i][j] = new PhanSo(parseFloat(tmp[0]))
       }
     }
-    d[i] = line[line.length - 2]
+    orgin.d[i] = line[line.length - 2]
     const tmp = line[line.length - 1].split('/')
     if (tmp.length === 2) {
-      pa[i] = new PhanSo(parseFloat(tmp[0]), parseFloat(tmp[1]))
+      orgin.pa[i] = new PhanSo(parseFloat(tmp[0]), parseFloat(tmp[1]))
     } else {
-      pa[i] = new PhanSo(parseFloat(tmp[0]))
+      orgin.pa[i] = new PhanSo(parseFloat(tmp[0]))
     }
-    if (pa[i].giatri < 0) {
-      pa[i].tu *= -1
-      for (let j = 0; j < line.length - 2; j++) {
-        rb[i][j].tu *= -1
+  }
+  orgin.n = maxLine
+  for (let i = 0; i < orgin.n; i++) {
+    if (orgin.fx[i] === undefined) {
+      orgin.fx[i] = new PhanSo()
+    }
+  }
+
+  orgin.type = fxType.trim()
+  return orgin
+}
+
+function setMatrix (lp) {
+  const rb = Object.assign(new LinearProgramming(), lp)
+  for (let i = 0; i < rb.n; i++) {
+    if (rb.rbdau[i] === -1) {
+      rb.fx[i].tu *= -1
+    }
+  }
+  for (let i = 0; i < rb.m; i++) {
+    if (rb.pa[i].giatri < 0) {
+      rb.pa[i].tu *= -1
+      rb.d[i] = (rb.d[i] === '>=' ? '<=' : '>=')
+      for (let j = 0; j < rb.n; j++) {
+        rb.rb[i][j].tu *= -1
       }
-      if (d[i] === '<=') d[i] = '>='
-      else if (d[i] === '>=') d[i] = '<='
     }
   }
-  n = maxLine
-  n = n + all
-  if (fxType === 'max') {
-    for (let i = 0; i < n; i++) {
-      fx[i].tu *= -1
+  let before = 0
+  for (let i = 0; i < rb.n; i++) {
+    let check = 0
+    for (let j = 0; j < rb.m; j++) {
+      if (rb.rbdau[i] === -1) {
+        rb.rb[j][i].tu *= -1
+      }
+      if (rb.rbdau[i] === undefined) {
+        check = 1
+        if (rb.fx[rb.n + before] === undefined) { rb.fx[rb.n + before] = new PhanSo(rb.fx[i].tu * -1) }
+        rb.rb[j][rb.n + before] = new PhanSo(rb.rb[j][i].tu * -1)
+      }
+    }
+    if (rb.rbdau[i] === undefined) {
+      rb.freeVar.push(rb.n + before)
+    }
+    if (check === 1) { before += 1 }
+  }
+  rb.n = rb.n + before
+  if (rb.type === 'Max') {
+    for (let i = 0; i < rb.n; i++) {
+      rb.fx[i].tu *= -1
     }
   }
-  for (let i = 0; i < n; i++) {
-    if (fx[i] === undefined) { fx[i] = new PhanSo() }
+
+  for (let i = 0; i < rb.n; i++) {
+    if (rb.fx[i] === undefined) { rb.fx[i] = new PhanSo() }
   }
-  rb[m] = [] // delta
+  rb.rb[rb.m] = [] // delta
   return rb
 }
 
-function setrb (rb, m, n) {
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      rb[m][j] = new PhanSo()
-      hsM[j] = new PhanSo()
-      if (fx[j] === undefined) {
-        fx[j] = new PhanSo()
+function setrb (rb) {
+  for (let i = 0; i < rb.m; i++) {
+    for (let j = 0; j < rb.n; j++) {
+      rb.rb[rb.m][j] = new PhanSo()
+      rb.hsM[j] = new PhanSo()
+      if (rb.fx[j] === undefined) {
+        rb.fx[j] = new PhanSo()
       }
-      if (rb[i][j] === undefined) {
-        rb[i][j] = new PhanSo()
+      if (rb.rb[i][j] === undefined) {
+        rb.rb[i][j] = new PhanSo()
       }
     }
   }
   return rb
 }
 
-function calculateDelta (n, m, hs) {
+function calculateDelta (rb, hs) {
   let posOfMaxDelta = 0
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < rb.n; i++) {
     let hs_M = new PhanSo(); let cons = new PhanSo()
-    for (let j = 0; j < m; j++) {
-      if (fx[hs[j]].giatri !== 123456) {
-        cons = Sum(cons, Multi(fx[hs[j]], rb[j][i]))
+    for (let j = 0; j < rb.m; j++) {
+      if (rb.fx[hs[j]].giatri != 99999999) {
+        cons = Sum(cons, Multi(rb.fx[hs[j]], rb.rb[j][i]))
       } else {
-        hs_M = Sum(hs_M, rb[j][i])
+        hs_M = Sum(hs_M, rb.rb[j][i])
       }
     }
-    rb[m][i] = fx[i].giatri !== 123456 ? Sub(cons, fx[i]) : cons
+    rb.rb[rb.m][i] = rb.fx[i].giatri != 99999999 ? Sub(cons, rb.fx[i]) : cons
     const hs_M1 = new PhanSo(1)
-    hsM[i] = fx[i].giatri !== 123456 ? hs_M : Sub(hs_M, hs_M1)
-    if (hsM[i].giatri > hsM[posOfMaxDelta].giatri || (hsM[i].giatri === hsM[posOfMaxDelta].giatri && rb[m][posOfMaxDelta].giatri < rb[m][i].giatri)) {
+    rb.hsM[i] = rb.fx[i].giatri != 99999999 ? hs_M : Sub(hs_M, hs_M1)
+    if (rb.hsM[i].giatri > rb.hsM[posOfMaxDelta].giatri || (rb.hsM[i].giatri === rb.hsM[posOfMaxDelta].giatri && rb.rb[rb.m][posOfMaxDelta].giatri < rb.rb[rb.m][i].giatri)) {
       posOfMaxDelta = i
     }
   }
   return posOfMaxDelta
 }
 
-function checkAlNegative (n, m, delta) {
+function checkAlNegative (rb, delta) {
   for (let i = 0; i < delta.length; i++) {
     let negative = 0
-    for (let j = 0; j < m; j++) {
-      if (rb[j][delta[i]].giatri <= 0) {
+    for (let j = 0; j < rb.m; j++) {
+      if (rb.rb[j][delta[i]].giatri <= 0) {
         ++negative
       }
     }
-    if (negative === m) {
+    if (negative === rb.m) {
       return false
     }
   }
   return true
 }
 
-function checkDelta (n, m) {
+function checkDelta (rb) {
   let pa_tu = 1
   const delta = []
-  for (let i = 0; i < n; i++) {
-    if (hsM[i].giatri > 0 || (!hsM[i].giatri && rb[m][i].giatri > 0)) {
+  for (let i = 0; i < rb.n; i++) {
+    if (rb.hsM[i].giatri > 0 || (!rb.hsM[i].giatri && rb.rb[rb.m][i].giatri > 0)) {
       pa_tu = 0
       delta.push(i)
     }
   }
   if (pa_tu) {
     return 1
-  } else if (!checkAlNegative(n, m, delta)) {
+  } else if (!checkAlNegative(rb, delta)) {
     return 0
   }
   return 2
 }
 
-function addBase () {
+function addBase (rb) {
   const hs = []
-  for (let i = 0; i < m; i++) {
+  for (let i = 0; i < rb.m; i++) {
     hs[i] = -1
-    if (d[i] === '>=') {
-      subVar.push(i)
-      rb[i][n++] = new PhanSo(parseFloat(-1))
-    } else if (d[i] === '<=') {
-      subVar.push(i)
-      rb[i][n++] = new PhanSo(parseFloat(1))
+    if (rb.d[i] === '>=') {
+      rb.subVar.push(i)
+      rb.rb[i][rb.n++] = new PhanSo(parseFloat(-1))
+    } else if (rb.d[i] === '<=') {
+      rb.subVar.push(i)
+      rb.rb[i][rb.n++] = new PhanSo(parseFloat(1))
     }
   }
-  rb = setrb(rb, m, n)
+  rb = setrb(rb)
   let varbase = 0
   // Tìm ẩn cơ sở
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < rb.n; i++) {
     let cnt_one = 0; let cnt_zero = 0
     let posj = 0
-    for (let j = 0; j < m; j++) {
-      if (rb[j][i].giatri === 1) {
+    for (let j = 0; j < rb.m; j++) {
+      if (rb.rb[j][i].giatri === 1) {
         posj = j
         ++cnt_one
-      } else if (rb[j][i].giatri === 0) {
+      } else if (rb.rb[j][i].giatri === 0) {
         ++cnt_zero
       }
     }
 
-    if (cnt_one === 1 && cnt_zero === m - 1) {
+    if (cnt_one === 1 && cnt_zero === rb.m - 1) {
       hs[posj] = i
       varbase += 1
     }
   }
-  if (varbase < m) {
-    for (let i = 0; i < m; i++) {
+  if (varbase < rb.m) {
+    for (let i = 0; i < rb.m; i++) {
       if (hs[i] === -1) {
-        fakeVar.push(n)
-        fx[n] = new PhanSo(parseFloat(123456))
-        hs[i] = n
-        rb[i][n++] = new PhanSo(parseFloat(1))
+        rb.fakeVar.push(rb.n)
+        rb.fx[rb.n] = new PhanSo(parseFloat(99999999))
+        hs[i] = rb.n
+        rb.rb[i][rb.n++] = new PhanSo(parseFloat(1))
       }
     }
   }
-  rb = setrb(rb, m, n)
+  rb = setrb(rb)
   return hs
 }
 
 function findMin (rb, posOfMaxDelta) {
   let minlet = parseFloat(10000)
   let posOfMin = 0
-  for (let j = 0; j < m; j++) {
-    if (rb[j][posOfMaxDelta].giatri > 0) {
-      if (pa[j].giatri / rb[j][posOfMaxDelta].giatri < minlet) {
-        minlet = pa[j].giatri / rb[j][posOfMaxDelta].giatri
+  for (let j = 0; j < rb.m; j++) {
+    if (rb.rb[j][posOfMaxDelta].giatri > 0) {
+      if (rb.pa[j].giatri / rb.rb[j][posOfMaxDelta].giatri < minlet) {
+        minlet = rb.pa[j].giatri / rb.rb[j][posOfMaxDelta].giatri
         posOfMin = j
       }
     }
@@ -254,65 +278,76 @@ function findMin (rb, posOfMaxDelta) {
   return posOfMin
 }
 
-function calculateFx (fx, x) {
+function calculateFx (x, rb) {
   let pa_result = new PhanSo()
-  for (let i = 0; i < orginN; i++) {
-    pa_result = Sum(pa_result, Multi(fx[i], x[i]))
+  for (let i = 0; i < rb.orginN; i++) {
+    pa_result = Sum(pa_result, Multi(rb.fx[i], x[i]))
   }
   pa_result.RutGon()
   return pa_result
 }
 
-function Savesol (hs) {
+function Savesol (hs, lp) {
   const res = []
-  for (let i = 0; i < m + 1; i++) {
+  for (let i = 0; i < lp.m + 1; i++) {
     const tmp = []
-    if (i < m) {
-      if (fx[hs[i]].giatri !== 99999999) {
-        tmp.push(fx[hs[i]].giatri)
+    if (i < lp.m) {
+      if (lp.fx[hs[i]].giatri !== 99999999) {
+        tmp.push(lp.fx[hs[i]].giatri)
       } else {
         tmp.push('M')
       }
 
       tmp.push(hs[i] + 1)
-      tmp.push(decimalToFraction(pa[i].giatri))
+      tmp.push(lp.pa[i].tu.toString() + lp.pa[i].mau.toString)
     }
 
-    for (let j = 0; j < n; j++) {
-      tmp.push(decimalToFraction(rb[i][j].giatri))
+    for (let j = 0; j < lp.n; j++) {
+      tmp.push(lp.rb[i][j].tu.toString() + lp.rb[i][j].mau.toString())
     }
     res.push(tmp)
   }
   return res
 }
 
-function print (hs, fxType) {
+function print (hs, fxType, lp) {
   const x = []
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < lp.n; i++) {
     x[i] = new PhanSo()
   }
-  for (let i = 0; i < m; i++) {
-    x[hs[i]].tu = pa[i].tu
-    x[hs[i]].mau = pa[i].mau
+  for (let i = 0; i < lp.m; i++) {
+    x[hs[i]].tu = lp.pa[i].tu
+    x[hs[i]].mau = lp.pa[i].mau
   }
 
-  for (let i = 0; i < fakeVar.length; i++) {
-    if (x[fakeVar[i]].giatri !== 0) {
+  for (let i = 0; i < lp.fakeVar.length; i++) {
+    if (x[lp.fakeVar[i]].giatri !== 0) {
       return failed
     }
   }
+  for (let i = 0; i < lp.n; i++) {
+    if (lp.rbdau[i] === -1) { x[i].tu *= -1; lp.fx[i].tu *= -1 }
+  }
 
   const res = { success: true, result: { fx: 0, x: [] } }
-  const pa_result = calculateFx(fx, x)
+  let pa_result = calculateFx(x, lp)
 
-  if (fxType === 'max') {
+  if (lp.type === 'max') {
+    for (let i = 0; i < lp.n; i++) {
+      lp.fx[i].tu *= -1
+    }
     pa_result.tu *= -1
   }
 
+  if (lp.freeVar.length !== 0) {
+    for (let i = 0; i < lp.freeVar.length; i++) {
+      pa_result = Sum(pa_result, Multi(lp.fx[freeVar[i]], x[freeVar[i]]))
+    }
+  }
   res.result.fx = pa_result.tu.toString() + '/' + pa_result.mau.toString()
 
   // let s = pa_result.tu.toString() + '/' + pa_result.mau.toString() + '<br>'
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < lp.n; i++) {
     res.result.x.push(x[i].tu.toString() + '/' + x[i].mau.toString())
     // s = s + x[i].tu.toString() + '/' + x[i].mau.toString() + ' '
   }
@@ -320,11 +355,9 @@ function print (hs, fxType) {
 }
 
 function processing (paramFx, paramMatrix, paramRB, paramFxType) {
-  fx = inputFx(paramFx)
-  rb = inputMatrix(paramMatrix, paramRB, paramFxType)
-  orginN = n
-
-  const hs = addBase()
+  let rb = input(paramFx, paramMatrix, paramRB, paramFxType)
+  rb =  setMatrix(rb)
+  const hs = addBase(rb)
   const allS = []
   let s = ''
   const mp = []
@@ -335,9 +368,9 @@ function processing (paramFx, paramMatrix, paramRB, paramFxType) {
   if (mp[s] === undefined) { mp[s] = 0 }
   mp[s] += 1
   while (true) {
-    let posOfMaxDelta = calculateDelta(n, m, hs)
-    allS.push(Savesol(hs))
-    const checkDeltaNow = checkDelta(n, m)
+    let posOfMaxDelta = calculateDelta(rb, hs)
+    allS.push(Savesol(hs, lp))
+    const checkDeltaNow = checkDelta(rb)
     if (checkDeltaNow === 0) {
       res.answer = failed
       return res
@@ -357,34 +390,34 @@ function processing (paramFx, paramMatrix, paramRB, paramFxType) {
     mp[s] += 1
     if (mp[s] !== 1) {
       posOfMaxDelta = 0
-      for (let i = 0; i < n; ++i) {
-        if ((hsM[i].giatri < hsM[posOfMaxDelta].giatri) || (hsM[i].giatri === hsM[posOfMaxDelta].giatri && rb[m][posOfMaxDelta].giatri > rb[m][i].giatri && rb[m][i].giatri > 0)) {
+      for (let i = 0; i < rb.n; ++i) {
+        if ((rb.hsM[i].giatri < rb.hsM[posOfMaxDelta].giatri) || (rb.hsM[i].giatri == rb.hsM[posOfMaxDelta].giatri && rb.rb[rb.m][posOfMaxDelta].giatri > rb.rb[rb.m][i].giatri && rb.rb[rb.m][i].giatri > 0)) {
           posOfMaxDelta = i
         }
       }
       posOfMinVar = findMin(rb, posOfMaxDelta)
       hs[posOfMinVar] = posOfMaxDelta
     }
-    const minValue = rb[posOfMinVar][posOfMaxDelta]
-    for (let i = 0; i < m; i++) {
-      if (i !== posOfMinVar) {
-        pa[i] = Div(Sub(Multi(pa[i], minValue), Multi(pa[posOfMinVar], rb[i][posOfMaxDelta])), minValue)
-        for (let j = 0; j < n; j++) {
-          if (j !== posOfMaxDelta) {
-            rb[i][j] = Div(Sub(Multi(rb[i][j], minValue), Multi(rb[posOfMinVar][j], rb[i][posOfMaxDelta])), minValue)
+    const minValue = rb.rb[posOfMinVar][posOfMaxDelta]
+    for (let i = 0; i < rb.m; i++) {
+      if (i != posOfMinVar) {
+        rb.pa[i] = Div(Sub(Multi(rb.pa[i], minValue), Multi(rb.pa[posOfMinVar], rb.rb[i][posOfMaxDelta])), minValue)
+        for (let j = 0; j < rb.n; j++) {
+          if (j != posOfMaxDelta) {
+            rb.rb[i][j] = Div(Sub(Multi(rb.rb[i][j], minValue), Multi(rb.rb[posOfMinVar][j], rb.rb[i][posOfMaxDelta])), minValue)
           }
         }
       }
     }
 
-    for (let i = 0; i < m; i++) {
-      if (i !== posOfMinVar) {
-        rb[i][posOfMaxDelta].init()
+    for (let i = 0; i < rb.m; i++) {
+      if (i != posOfMinVar) {
+        rb.rb[i][posOfMaxDelta].init()
       }
     }
-    pa[posOfMinVar] = Div(pa[posOfMinVar], rb[posOfMinVar][posOfMaxDelta])
-    for (let i = 0; i < n; ++i) {
-      rb[posOfMinVar][i] = Div(rb[posOfMinVar][i], minValue)
+    rb.pa[posOfMinVar] = Div(rb.pa[posOfMinVar], rb.rb[posOfMinVar][posOfMaxDelta])
+    for (let i = 0; i < rb.n; ++i) {
+      rb.rb[posOfMinVar][i] = Div(rb.rb[posOfMinVar][i], minValue)
     }
   }
 }
